c858fa57cf7aeb79ea6b52b616dfee1d1ad807f5
Josh Hathcock <jdspiral@gmail.com>
2025-04-15 13:53:21 -0400

    Fix typo

--- app/api/subscriptions/[id]/route.ts
+++ app/api/subscriptions/[id]/route.ts
@@ -90,24 +90,24 @@ export async function GET(
 }
 
 export async function PUT(
-    request: Request,
-    { params }: { params: { id: string } }
-  ) {
-    const id = params.id;
-    try {
-      const body = await request.json();
-      const { data, error } = await supabase
-        .from('subscriptions')
-        .update(body)
-        .eq('id', id);
-      if (error) {
-        return NextResponse.json({ error: error.message }, { status: STATUS.SERVER_ERROR });
-      }
-      return NextResponse.json(data, { status: STATUS.OK });
-    } catch (err: any) {
-      return NextResponse.json({ error: err.message }, { status: STATUS.SERVER_ERROR });
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const id = params.id;
+  try {
+    const body = await request.json();
+    const { data, error } = await supabase
+      .from('subscriptions')
+      .update(body)
+      .eq('id', id);
+    if (error) {
+      return NextResponse.json({ error: error.message }, { status: STATUS.SERVER_ERROR });
     }
+    return NextResponse.json(data, { status: STATUS.OK });
+  } catch (err: any) {
+    return NextResponse.json({ error: err.message }, { status: STATUS.SERVER_ERROR });
   }
+}
   
 export async function DELETE(
   request: Request,
